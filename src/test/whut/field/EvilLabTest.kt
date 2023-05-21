572a779c5d1f2743ad34eafab52919d9a2a889f6
Levi0727 <molnar.levente11@gmail.com>
2023-05-21 12:52:03 +0200

    test: add FieldTest.kt and EvilLabTest.kt
    #2

---
+++ src/test/whut/field/EvilLabTest.kt
@@ -0,0 +1,53 @@
+package whut.field
+
+import org.junit.jupiter.api.AfterAll
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.BeforeAll
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.mockito.MockedStatic
+import org.mockito.Mockito.*
+import whut.player.Game
+import whut.player.Virologus
+import whut.ui.control.MyRunnable
+
+class EvilLabTest {
+    private lateinit var evilLab: EvilLab
+
+    companion object {
+        private lateinit var myRunnableMock: MockedStatic<MyRunnable>
+
+        @BeforeAll
+        @JvmStatic
+        fun setup() {
+            val game = mock(Game::class.java)
+            doNothing().`when`(game).bearAll()
+
+            myRunnableMock = mockStatic(MyRunnable::class.java)
+            `when`(MyRunnable.getGame()).thenReturn(game)
+        }
+
+        @AfterAll
+        @JvmStatic
+        fun cleanup(){
+            myRunnableMock.close()
+        }
+    }
+
+    @BeforeEach
+    fun initTest(){
+        evilLab = EvilLab()
+    }
+
+    @Test
+    fun touchingEvilLab(){
+        val virologist = Virologus()
+        evilLab.touching(virologist)
+        assertEquals(1, virologist.agensOnMe.size)
+    }
+
+    @Test
+    fun testToString(){
+        assertEquals("evillab", evilLab.toString())
+    }
+}
---
+++ src/test/whut/field/FieldTest.kt
@@ -0,0 +1,98 @@
+package whut.field
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+import whut.genetic_code.ForgetCode
+import whut.item.Item
+import whut.player.Entity
+import whut.player.Virologus
+import java.lang.IndexOutOfBoundsException
+
+class FieldTest {
+    private lateinit var field: Field
+
+    @BeforeEach
+    fun init(){
+        field = Field()
+    }
+
+    @Test
+    fun testConstructor(){
+        assertEquals(0, field.neighbourhood.size)
+        assertEquals(0, field.virologusok.size)
+    }
+
+    @Test
+    fun setNeighborToField() {
+        val neighborField = Field()
+        field.setNeighbour(neighborField)
+        assertEquals(field.neighbourhood.size, 1)
+        assertEquals(neighborField.neighbourhood.size, 1)
+    }
+
+    @Test
+    fun addItemToField(){
+        field.addItem(Item())
+        assertEquals(0, field.items.size)
+        assertEquals(null, field.getItem(""))
+    }
+
+    @Test
+    fun removeItemFromField(){
+        val item = Item()
+        field.addItem(item)
+        field.removeItem(item)
+        assertEquals(0, field.items.size)
+        assertEquals(null, field.getItem(""))
+    }
+
+    @Test
+    fun setGeneticCodeToField(){
+        field.setGeneticCode(ForgetCode())
+        assertEquals(null, field.codeHere())
+    }
+
+    @Test
+    fun acceptVirologistToField(){
+        val virologist = Virologus()
+        field.accept(virologist)
+        assertEquals(virologist, field.au[0])
+        assertEquals(virologist.field, field)
+    }
+
+    @Test
+    fun acceptEntityToField(){
+        val entity = Entity()
+        val exception = assertThrows<ClassCastException> {
+            field.accept(entity)
+        }
+        assertEquals("whut.player.Entity cannot be cast to whut.player.Virologus", exception.message)
+    }
+
+    @Test
+    fun removeVirologistFromField(){
+        val virologist = Virologus()
+        field.accept(virologist)
+        field.remove(virologist)
+        assertEquals(0, field.au.size)
+    }
+
+    @Test
+    fun removeEntityFromField(){
+        assertEquals(0, field.au.size)
+        field.remove(Entity())
+        assertEquals(0, field.au.size)
+    }
+
+    @Test
+    fun getPacketFromField(){
+        assertEquals(null, field.packet)
+    }
+
+    @Test
+    fun testToString(){
+        assertEquals("whut/field", field.toString())
+    }
+}
